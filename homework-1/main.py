c85022f845a3c4c28b2daf0df74949d42ab93afa
Rasuljka <ras199167@gmail.com>
2024-02-24 01:04:03 +0500

    hw1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='1234'
+)
+
+cur = conn.cursor()
+
+with open('north_data/employees_data.csv', 'r') as f:
+    """Чтение данных из CSV-файла и загрузка их в базу данных"""
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute(
+            "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+            row
+        )
+
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute(
+            "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+            row
+        )
+
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute(
+            "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+            row
+        )
+
+conn.commit()
+cur.close()
+conn.close()
